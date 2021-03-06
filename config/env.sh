245f3b10a18400bbca068f0ccf46783b7f680b7c
Rodrigo Lopez Alvarado <rodrigo.lopez.160795@hotmail.com>
2021-03-06 11:17:38 -0600

    Borrandoo

--- config/env.sh
+++
@@ -1,4 +0,0 @@
-export NODE_ENV='production'
-export PORT=3000
-export SECRET='secret'
-export MONGO_URI='mongodb+srv://User:34PcU5HtHEv6q0mG@cluster0.xhkcc.mongodb.net/users_database?retryWrites=true&w=majority'
