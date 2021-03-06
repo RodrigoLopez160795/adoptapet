92477333d11486f75d099503c4c1bf9007fa3627
Rodrigo Lopez Alvarado <rodrigo.lopez.160795@hotmail.com>
2021-03-06 11:48:54 -0600

    Cambiando url mongo

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret',
     mongo : {
-        uri: process.env.NODE_ENV === 'production' ? process.env.MONGO_URI : 'mongodb://localhost:27017/mydbname'
+        uri: process.env.NODE_ENV === 'production' ? process.env.MONGO_URI : 'mongodb+srv://User:34PcU5HtHEv6q0mG@cluster0.xhkcc.mongodb.net/users_database?retryWrites=true&w=majority'
     }
 }
 
