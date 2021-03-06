9b0e6143af00695403376df0028c158bacc2ed10
Rodrigo Lopez Alvarado <rodrigo.lopez.160795@hotmail.com>
2021-03-06 11:30:30 -0600

    final

---
+++ config/env.sh
@@ -0,0 +1,4 @@
+export NODE_ENV='production'
+export PORT=3000
+export SECRET='secret'
+export MONGO_URI='mongodb+srv://User:34PcU5HtHEv6q0mG@cluster0.xhkcc.mongodb.net/users_database?retryWrites=true&w=majority'
